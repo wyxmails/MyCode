a1ae641de331740c02dc27b88709f305c310776a
wyxmails <wyxmails@gmail.com>
2014-05-27 13:40:23 -0400

    Distinct Subsequences

---
+++ leetcode/distinctSub.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+/*
+ Given a string S and a string T, count the number of distinct subsequences of T in S.
+ 
+ A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+ 
+ Here is an example:
+ S = "rabbbit", T = "rabbit"
+ 
+ Return 3.
+ */
+int numDistinct(string S, string T) {
+	int n = S.size();
+	int m = T.size();
+	int arr[m+1][n+1];
+	for(int i=0;i<=m;++i){
+		for(int j=0;j<=n;++j)
+			if(i==0) arr[i][j] = 1;
+			else arr[i][j] = 0;
+	}
+	for(int i=0;i<m;++i){
+		for(int j=0;j<n;++j){
+			if(S[j]==T[i]){
+				arr[i+1][j+1] = arr[i][j]+arr[i+1][j];
+			}else arr[i+1][j+1] = arr[i+1][j];
+		}
+	}
+	return arr[m][n];
+}
+int main(int argc,char*argv[]){
+	string s = "ccc";
+	string t = "c";
+	cout << numDistinct(s,t) << endl;
+	return 0;
+}
