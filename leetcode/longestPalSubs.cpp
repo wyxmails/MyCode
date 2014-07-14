55565f2c11535e0eee98cae62090b5695ecab4fc
wyxmails <wyxmails@gmail.com>
2014-07-14 15:35:24 -0400

    Update longestPalSubs.cpp
    longestPalindrome2 ac

--- leetcode/longestPalSubs.cpp
+++ leetcode/longestPalSubs.cpp
@@ -28,3 +28,40 @@ string longestPalindrome(string s) {
 	}
 	return res;
 }
+bool isP(const string&s,int l,int r){
+        while(r>l){
+            if(s[r]!=s[l]) return false;
+            r--; l++;
+        }
+        return true;
+    }
+string longestPalindrome2(string s) {
+        int n = s.size();
+        if(n<=1) return s;
+        vector<int> len(n,1);
+        int end = n-2;
+        int Max = INT_MIN;
+        int begin;
+        while(end>=0){
+            if(end+len[end+1]+1<n&&s[end]==s[end+len[end+1]+1]){
+                len[end] = len[end+1]+2;
+                if(len[end]>Max){
+                    Max = len[end];
+                    begin = end;
+                }
+            }else{
+                for(int i=end+len[end+1];i>end;--i){
+                    if(isP(s,end,i)){
+                        len[end] = i-end+1;
+                        if(len[end]>Max){
+                            Max = len[end];
+                            begin = end;
+                        }
+                        break;
+                    }
+                }
+            }
+            end--;
+        }
+        return s.substr(begin,Max);
+    }
