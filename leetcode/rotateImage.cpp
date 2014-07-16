9896136b27860002957efb1e270e43563129c297
wyxmails <wyxmails@gmail.com>
2014-07-16 17:42:41 -0400

    Update rotateImage.cpp

--- leetcode/rotateImage.cpp
+++ leetcode/rotateImage.cpp
@@ -24,3 +24,23 @@ void rotate(vector<vector<int> > &matrix) {
 		}
 	}
 }
+void rotate2(vector<vector<int> > &matrix) {
+        int n = matrix.size();
+        if(n==0) return;
+        //diagonal 
+        for(int i=0;i<n;++i){
+            for(int j=0;j<i;++j){
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+        //horizon
+        for(int i=0;i<n;++i){
+            for(int j=0;j<(n+1)/2;++j){
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = tmp;
+            }
+        }
+}
