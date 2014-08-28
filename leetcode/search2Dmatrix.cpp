4bbe1c30bbda6e4d7489423e74043993ee37cb0a
wyxmails <wyxmails@gmail.com>
2014-08-27 21:54:23 -0400

    Search a 2D Matrix

--- leetcode/search2Dmatrix.cpp
+++ leetcode/search2Dmatrix.cpp
@@ -34,6 +34,22 @@ bool searchMatrix(vector<vector<int> > &matrix, int target) {
 		if(target==matrix[i][j]) return true;
 	return false;
 }
+
+bool searchMatrix2(vector<vector<int> > &matrix, int target) {
+        int m = matrix.size();
+        if(m==0) return false;
+        int n = matrix[0].size();
+        if(n==0) return false;
+        int i=0;
+        int j=n-1;
+        while(i<m&&j>=0){
+            if(target==matrix[i][j]) return true;
+            else if(target<matrix[i][j]) j--;
+            else i++;
+        }
+        return false;
+}
+
 int main(int argc,char *argv[]){
 	return 0;
 }
