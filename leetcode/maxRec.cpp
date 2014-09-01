3fe023ff12c19eafa228ba26b3f3e518878a9aab
wyxmails <wyxmails@gmail.com>
2014-09-01 17:05:20 -0400

    Update maxRec.cpp

--- leetcode/maxRec.cpp
+++ leetcode/maxRec.cpp
@@ -35,6 +35,37 @@ int maximalRectangle(vector<vector<char> > &matrix) {
 	}
 	return M;
 }
+
+class Solution2 {
+public:
+    int maximalRectangle(vector<vector<char> > &matrix) {
+        int m = matrix.size();
+        if(m==0) return 0;
+        int n = matrix[0].size();
+        if(n==0) return 0;
+        vector<vector<int> > mark(m+1,vector<int>(n+1,0));
+        for(int i=0;i<m;++i){
+            for(int j=0;j<n;++j){
+                if(matrix[i][j]=='1') mark[i][j+1] = mark[i][j]+1;
+            }
+        }
+        int M=0;
+        for(int j=1;j<=n;++j){
+            stack<int> myS;
+            for(int i=0;i<=m;){
+                if(myS.empty()||mark[i][j]>=mark[myS.top()][j]) myS.push(i++);
+                else{
+                    int tmp = myS.top();
+                    myS.pop();
+                    int l = myS.empty()?0:myS.top()+1;
+                    M = max(mark[tmp][j]*(i-l),M);
+                }
+            }
+        }
+        return M;
+    }
+};
+
 int main(){
 	vector<vector<char> > test;
 	vector<char> vec;
