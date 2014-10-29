6af13746a5b7adc927eb7578d765575b1c754d6c
wyxmails <wyxmails@gmail.com>
2014-10-29 14:57:08 -0400

    141029

--- crackCode_5/09.03.cpp
+++ crackCode_5/09.03.cpp
@@ -19,6 +19,24 @@ int MagicIndex(vector<int> A){
 	return -1;
 }
 
+int MagicIndexDup(const vector<int>&A,int l,int r){
+	 if(l>r) return -1;
+	 int m = (l+r)/2;
+	 if(A[m]==m) return m;
+	 int mr = min(m-1,A[m]);
+	 int resl = MagicIndexDup(A,l,mr);
+	 if(resl!=-1) return resl;
+	 int ml = max(m+1,A[m]);
+	 int resr = MagicIndexDup(A,ml,r);
+	 if(resr!=-1) return resr;
+	 return -1;
+}
+
+int MagicIndexDup(vector<int> A){
+	int l=0,r=A.size()-1;
+	return MagicIndexDup(A,l,r);
+}
+
 int main(int argc,char *argv[]){
 	srand(time(NULL));
 	int n = rand()%10+2;
@@ -38,5 +56,13 @@ int main(int argc,char *argv[]){
 	cout << endl;
 	cout << "m: " << m << endl;
 	cout << MagicIndex(vec) << endl;
+	int arr[] = {-1,0,1,3,3,4,4,5,6,7};
+	vector<int> vec1;
+	for(int i=0;i<sizeof(arr)/sizeof(arr[0]);++i){
+		cout << arr[i] << " ";
+		vec1.push_back(arr[i]);
+	}
+	cout << endl;
+	cout << MagicIndexDup(vec1) << endl;
 	return 0;
 }
