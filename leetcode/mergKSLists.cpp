77d52c4186bd9e1e0f315705460cac1711deebff
wyxmails <wyxmails@gmail.com>
2014-11-13 22:08:30 -0500

    141113

--- leetcode/mergKSLists.cpp
+++ leetcode/mergKSLists.cpp
@@ -3,63 +3,39 @@
    Analyze and describe its complexity.
    my solution: if total number is n, the complexity is O(k*n).
  */
-ListNode *mergeKLists(vector<ListNode *> &lists) {
-	// Start typing your C/C++ solution below
-	// DO NOT write int main() function
-	ListNode*head,*tail;
-	head=tail=NULL;
-	if(lists.size()==0) return head;
-	if(lists.size()==1) return lists[0];
-	int mini;
-	while(lists.size()){
-		mini=0;
-		while(lists.size()&&lists[mini]==NULL){
-			lists.erase(lists.begin()+mini);
-		}
-		for(int i=mini+1;i<lists.size();++i){
-			if(lists[i]!=NULL&&lists[i]->val<lists[mini]->val) mini = i;
-		}
-		if(lists.size()){
-			if(tail==NULL){
-				head = tail = lists[mini];
-			}else{
-				tail->next = lists[mini];
-				tail = lists[mini];
-			}
-			lists[mini] = (*lists[mini]).next;
-			if(lists[mini]==NULL) lists.erase(lists.begin()+mini);
-		}
-	}
-	return head;
-}
-ListNode *mergeKLists(vector<ListNode *> &lists) {
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
         int n = lists.size();
-        if(n==0) return NULL;
-        ListNode *head = lists[0];
-        for(int i=1;i<n;++i){
-            if(lists[i]==NULL) continue;
-            if(head==NULL){
-                head = lists[i];
-                continue;
-            } 
-            ListNode *cur1,*cur2,*pre;
-            cur1 = head;
-            cur2 = lists[i];
-            pre = NULL;
-            if(cur1->val>cur2->val) head = cur2;
-            while(cur1!=NULL&&cur2!=NULL){
-                if(cur1->val>cur2->val){
-                    if(pre!=NULL) pre->next = cur2;
-                    pre = cur2;
-                    cur2 = cur2->next;
-                }else{
-                    if(pre!=NULL) pre->next = cur1;
-                    pre = cur1;
-                    cur1 = cur1->next;
-                }
-            }
-            if(cur1!=NULL) pre->next = cur1;
-            if(cur2!=NULL) pre->next = cur2;
+        return merge(lists,0,n-1);
+    }
+    ListNode*merge(vector<ListNode*>&lists,int l,int r){
+        if(l>r) return NULL;
+        if(l==r) return lists[l];
+        if(r-l==1) return mergeTwo(lists[l],lists[r]);
+        int m = (l+r)/2;
+        ListNode *L = merge(lists,l,m);
+        ListNode *R = merge(lists,m+1,r);
+        return mergeTwo(L,R);
+    }
+    ListNode*mergeTwo(ListNode*l,ListNode*r){
+        if(l==NULL&&r==NULL) return NULL;
+        if(l==NULL) return r;
+        if(r==NULL) return l;
+        if(l->val<r->val){
+            l->next = mergeTwo(l->next,r);
+            return l;
+        }else{
+            r->next = mergeTwo(l,r->next);
+            return r;
         }
-        return head;
-}
+    }
+};
