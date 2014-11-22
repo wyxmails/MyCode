59af05eefd5d65679818df82ede753d498993e4e
wyxmails <wyxmails@gmail.com>
2014-11-21 22:09:15 -0500

    141121

--- leetcode/pathSum.cpp
+++ leetcode/pathSum.cpp
@@ -56,3 +56,16 @@ public:
         return false;
     }
 };
+
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode *root, int sum) {
+        if(root==NULL) return false;
+        if(root->left==NULL&&root->right==NULL)
+            return (root->val==sum);
+        if(root->left!=NULL&&hasPathSum(root->left,sum-root->val)) return true;
+        if(root->right!=NULL&&hasPathSum(root->right,sum-root->val)) return true;
+        return false;
+    }
+};
