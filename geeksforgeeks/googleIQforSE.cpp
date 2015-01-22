d2f98c8c76647a829da7261c4ec0acf6b551d974
wyxmails <wyxmails@gmail.com>
2015-01-22 16:55:37 -0500

    KMP

--- geeksforgeeks/googleIQforSE.cpp
+++ geeksforgeeks/googleIQforSE.cpp
@@ -91,6 +91,28 @@ bool isPattern2(string &S){
 	return false;
 }
 
+bool isPatternKMP(string&S){
+	int n = S.size();
+	vector<int> maton(n+1,0);
+	for(int i=1;i<n;++i){
+		int t =i;
+		while(t>0){
+			t = maton[t];
+			if(S[i]==S[t]) {maton[i+1] = t+1; break;}
+		}
+	}
+	int len = n-maton[n];
+	if(len<=1||len>n/2) return false;
+	int cur = n;
+	int next = maton[n];
+	while(next>0){
+		if(cur-next!=len) return false;
+		cur = next;
+		next = maton[cur];
+	}
+	return true;
+}
+
 int main(int argc,char*argv[]){
 	//cout << LCM(10,15) << endl;
 	//cout << LCM(12,16) << endl;
@@ -103,29 +125,66 @@ int main(int argc,char*argv[]){
 	string S = "abab";
 	if(isPattern2(S)) cout << "true" << endl;
 	else cout << "false" << endl;
+	cout << "KMP: ";
+	if(isPatternKMP(S)) cout << "true" << endl;
+	else cout << "false" << endl;
+	
 	S = "abcdabcd";
 	if(isPattern2(S)) cout << "true" << endl;
 	else cout << "false" << endl;
+	cout << "KMP: ";
+	if(isPatternKMP(S)) cout << "true" << endl;
+	else cout << "false" << endl;
+	
 	S = "abcabcabc";
 	if(isPattern2(S)) cout << "true" << endl;
 	else cout << "false" << endl;
+	cout << "KMP: ";
+	if(isPatternKMP(S)) cout << "true" << endl;
+	else cout << "false" << endl;
+	
 	S = "zzxzzxzzx";
 	if(isPattern2(S)) cout << "true" << endl;
 	else cout << "false" << endl;
+	cout << "KMP: ";
+	if(isPatternKMP(S)) cout << "true" << endl;
+	else cout << "false" << endl;
+	
 	S = "zzxxzzxxzzxx";
 	if(isPattern2(S)) cout << "true" << endl;
+	else cout << "false" << endl;
+	cout << "KMP: ";
+	if(isPatternKMP(S)) cout << "true" << endl;
 	else cout << "false" << endl;
 
 	cout << endl << "False test cases: " << endl;
 	S = "abac";
 	if(isPattern2(S)) cout << "true" << endl;
 	else cout << "false" << endl;
+	cout << "KMP: ";
+	if(isPatternKMP(S)) cout << "true" << endl;
+	else cout << "false" << endl;
+	
 	S = "abcdabbd";
 	if(isPattern2(S)) cout << "true" << endl;
 	else cout << "false" << endl;
+	cout << "KMP: ";
+	if(isPatternKMP(S)) cout << "true" << endl;
+	else cout << "false" << endl;
+
 	S = "abcabcefg";
 	if(isPattern2(S)) cout << "true" << endl;
 	else cout << "false" << endl;
+	cout << "KMP: ";
+	if(isPatternKMP(S)) cout << "true" << endl;
+	else cout << "false" << endl;
+	
 	S = "zzxzzyzzx";
+	if(isPattern2(S)) cout << "true" << endl;
+	else cout << "false" << endl;
+	cout << "KMP: ";
+	if(isPatternKMP(S)) cout << "true" << endl;
+	else cout << "false" << endl;
+	
 	return 0;
 }
