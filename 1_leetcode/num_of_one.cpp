f8d6a8be24835b44018db128bf8e3d7a7cdc830f
wyxmails <wyxmails@gmail.com>
2015-03-23 03:02:03 -0400

    Update num_of_one.cpp

--- 1_leetcode/num_of_one.cpp
+++ 1_leetcode/num_of_one.cpp
@@ -1,1 +1,17 @@
+/*
+Number of 1 Bits Total Accepted: 12135 Total Submissions: 32972 My Submissions Question Solution 
+Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
 
+For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
+*/
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int res = 0;
+        while(n>0){
+            res ++;
+            n &= (n-1);
+        }
+        return res;
+    }
+};
