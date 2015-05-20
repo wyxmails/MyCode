f0dec00c9f4c20a2c36f455affbe631d88872b97
wyxmails <wyxmails@gmail.com>
2015-05-20 21:27:38 +0800

    Create happyNumber.java

---
+++ 1_leetcode/happyNumber.java
@@ -0,0 +1,33 @@
+/*
+Happy Number Total Accepted: 11068 Total Submissions: 34481 My Submissions Question Solution 
+Write an algorithm to determine if a number is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+Example: 19 is a happy number
+
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+*/
+
+public class Solution {
+    public boolean isHappy(int n) {
+        HashSet hs = new HashSet();
+        int des = n;
+        hs.add(n);
+        while(des!=1){
+            int tmp = 0;
+            while(des>0){
+                tmp += (des%10)*(des%10);
+                des /= 10;
+            }
+            des = tmp;
+            if(des==1) break;
+            if(hs.contains(des)) return false;
+            hs.add(des);
+        }
+        return true;
+    }
+}
