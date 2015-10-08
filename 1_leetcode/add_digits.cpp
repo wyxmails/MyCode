bf6426b8682d6c1266e5d4c53bee8204afdbb4a5
wyxmails <wyxmails@gmail.com>
2015-10-08 22:05:49 +0800

    Update add_digits.cpp

--- 1_leetcode/add_digits.cpp
+++ 1_leetcode/add_digits.cpp
@@ -25,3 +25,19 @@ public:
         return num%9;
     }
 };
+
+
+class Solution1 {
+public:
+    int addDigits(int num) {
+        while(num>9){
+            int tmp = 0;
+            while(num>0){
+                tmp += num%10;
+                num /= 10;
+            }
+            num = tmp;
+        }
+        return num;
+    }
+};
