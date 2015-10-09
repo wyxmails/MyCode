b7cdbc77a886bb0404a1231f0d4e0883e3e6ddc9
wyxmails <wyxmails@gmail.com>
2015-10-09 22:47:46 +0800

    Create valid_anagram.cpp

---
+++ 1_leetcode/valid_anagram.cpp
@@ -0,0 +1,21 @@
+/*
+Valid Anagram My Submissions Question Solution 
+Total Accepted: 28754 Total Submissions: 79110 Difficulty: Easy
+Given two strings s and t, write a function to determine if t is an anagram of s.
+
+For example,
+s = "anagram", t = "nagaram", return true.
+s = "rat", t = "car", return false.
+
+Note:
+You may assume the string contains only lowercase alphabets.
+*/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return (s==t);
+    }
+};
