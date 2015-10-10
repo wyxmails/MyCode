a2bd14c65a35631511651a20818f3df057f797b2
wyxmails <wyxmails@gmail.com>
2015-10-10 23:20:47 +0800

    Update find_dup_num.cpp

--- 1_leetcode/find_dup_num.cpp
+++ 1_leetcode/find_dup_num.cpp
@@ -29,3 +29,21 @@ public:
         return left;
     }
 };
+
+/*a great problem and perfect solution*/
+class Solution1 {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow=0,fast=0;
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow!=fast);
+        int finder = 0;
+        do{
+            slow = nums[slow];
+            finder = nums[finder];
+        }while(slow!=finder);
+        return slow;
+    }
+};
