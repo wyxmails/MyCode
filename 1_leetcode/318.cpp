538106ce039be5b0d1abebf8f66223b97bd4e96d
wyxmails <wyxmails@gmail.com>
2016-01-23 21:33:44 +0800

    Update and rename maximum_product_of_word_lengths.cpp to 318.cpp

--- 1_leetcode/318.cpp
+++ 1_leetcode/318.cpp
@@ -1,7 +1,10 @@
 /*
 318. Maximum Product of Word Lengths My Submissions Question
 Total Accepted: 8710 Total Submissions: 22556 Difficulty: Medium
-Given a string array words, find the maximum value of length(word[i]) * length(word[j]) where the two words do not share common letters. You may assume that each word will contain only lower case letters. If no such two words exist, return 0.
+Given a string array words, find the maximum value of length(word[i]) * length(word[j]) 
+where the two words do not share common letters. 
+You may assume that each word will contain only lower case letters. 
+If no such two words exist, return 0.
 
 Example 1:
 Given ["abcw", "baz", "foo", "bar", "xtfn", "abcdef"]
