f32866b2ce1d534a08555f686d42f30e245d34ee
wyxmails <wyxmails@gmail.com>
2016-05-08 15:51:37 +0800

    Update 343.cpp

--- 1_leetcode/343.cpp
+++ 1_leetcode/343.cpp
@@ -37,3 +37,23 @@ public:
 private:
     int arr[1000];
 };
+
+/*
+Hint:
+
+There is a simple O(n) solution to this problem.
+You may check the breaking results of n ranging from 7 to 10 to discover the regularities.
+
+*/
+class Solution2 {
+public:
+    int integerBreak(int n) {
+        if(n<=3)
+            return n-1;
+        if(n%3==0)
+            return pow(3,n/3);
+        if(n%3==1)
+            return pow(3,n/3-1)*4;
+        return pow(3,n/3)*2;
+    }
+};
